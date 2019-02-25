11f3a147fead1404d8d3d2bd39f6a03925d12ca2
Sharad Vikram <sharad.vikram@gmail.com>
2019-02-25 02:06:55 -0800

    refactored example

--- example.py
+++ example.py
@@ -1,29 +1,28 @@
 import numpy as np
 import dr
 
-dist = dr.dist.Normal('Walker', 'mujoco', stdev=0.1)
+dist = dr.dist.Normal('Walker', 'dart', stdev=0.1)
 
 def sample_action():
     return np.random.uniform(low=env.action_space.low, high=env.action_space.high)
+
+def sample_trajectory():
+    env.reset()
+    env.render()
+    for _ in range(1000):
+        action = sample_action()
+        state, _, _, _ = env.step(action)
+        env.render()
 
 dist.seed(0)
 env = dist.sample()
 env.seed(0)
 np.random.seed(0)
-env.reset()
-env.render()
-for _ in range(1000):
-    action = sample_action()
-    state, _, _, _ = env.step(action)
-    env.render()
+sample_trajectory()
+
 
 dist.seed(1)
 env = dist.sample()
 env.seed(0)
 np.random.seed(0)
-env.reset()
-env.render()
-for _ in range(1000):
-    action = sample_action()
-    state, _, _, _ = env.step(action)
-    env.render()
+sample_trajectory()
