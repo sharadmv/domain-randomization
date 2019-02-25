aae2216d1a446301f87ba0b98dd6a4512577dc02
Sharad Vikram <sharad.vikram@gmail.com>
2019-02-25 02:01:46 -0800

    fixed random seeds

--- example.py
+++ example.py
@@ -1,14 +1,26 @@
 import numpy as np
 import dr
 
-dist = dr.dist.Normal('Walker', 'dart')
+dist = dr.dist.Normal('Walker', 'mujoco', stdev=0)
 
 def sample_action():
     return np.random.uniform(low=env.action_space.low, high=env.action_space.high)
 
 dist.seed(0)
-np.random.seed(0)
 env = dist.sample()
+env.seed(0)
+np.random.seed(0)
+env.reset()
+env.render()
+for _ in range(1000):
+    action = sample_action()
+    state, _, _, _ = env.step(action)
+    env.render()
+
+dist.seed(1)
+env = dist.sample()
+env.seed(0)
+np.random.seed(0)
 env.reset()
 env.render()
 for _ in range(1000):
