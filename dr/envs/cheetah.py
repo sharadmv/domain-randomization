0e08d5131720d94bc049ea15080716de3bb24e30
Sharad Vikram <sharad.vikram@gmail.com>
2019-02-27 18:20:30 -0800

    fixed walker/cheetah

--- dr/envs/cheetah.py
+++ dr/envs/cheetah.py
@@ -5,12 +5,20 @@ from dr.envs.driver import Driver
 class Cheetah(Driver):
 
     def get_parameters(self, env):
-        return np.concatenate([self.backend.get_masses(env), [self.backend.get_gravity(env)]])
+        return {
+            'mass': self.backend.get_masses(env),
+            'damping': self.backend.get_damping_coefficients(env),
+            'gravity': self.backend.get_gravity(env),
+        }
+        return np.concatenate([self.backend.get_masses(env)[:], self.backend.get_damping_coefficients(env), [self.backend.get_gravity(env)]])
 
     def set_parameters(self, env, parameters):
-        masses = parameters[:-1]
+        masses = parameters['mass']
+        gravity = parameters['gravity']
+        damping = parameters['damping']
+
         assert np.all(masses > 0), 'masses can not be 0 or negative'
 
-        gravity = parameters[-1]
         self.backend.set_masses(env, masses)
         self.backend.set_gravity(env, gravity)
+        self.backend.set_damping_coefficients(env, damping)
