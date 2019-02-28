d49b201ddb8d3175a84306c08448c130daae4da9
Quan Vuong <quan.vuong@nyu.edu>
2019-02-27 16:20:16 -0800

    implemented truncated normal sampling to ensure mass attributes are never set to 0 or negative value

--- dr/dist/normal.py
+++ dr/dist/normal.py
@@ -1,5 +1,7 @@
 import numpy as np
+import scipy.stats as stats
 from dr.dist.env_dist import EnvironmentDistribution
+
 
 class Normal(EnvironmentDistribution):
 
@@ -8,5 +10,24 @@ class Normal(EnvironmentDistribution):
         super(Normal, self).__init__(*args, **kwargs)
 
     def _sample(self, params):
-        params = np.random.normal(params, scale=self.scale)
-        return params
+
+        # Sample gravity parameter
+        g = np.random.normal(params[-1], scale=self.scale)
+
+        # Mass parameters can only be positive,
+        # We thus sample them from a truncated normal distribution
+        # We set the lower bound to an arbitrary small positive number
+        # since stats.truncnorm samples in closed interval [a, b].
+        # The distribution is theoretically un-bounded from above
+        # But we set the upper bound to be an arbitrary large number
+        # to avoid any potential numerical issue with setting upper bound to be +inf
+        mass_means = params[:-1]
+
+        X = stats.truncnorm(1e-7, 1e7, loc=mass_means, scale=self.scale)
+        mass = X.rvs(size=len(mass_means))
+
+        sample = np.concatenate((mass, [g]))
+
+        assert len(sample) == len(params)
+
+        return sample
--- dr/envs/walker.py
+++ dr/envs/walker.py
@@ -9,6 +9,8 @@ class Walker(Driver):
 
     def set_parameters(self, env, parameters):
         masses = parameters[:-1]
+        assert np.all(masses > 0), 'masses can not be 0 or negative'
+
         gravity = parameters[-1]
         self.backend.set_masses(env, masses)
         self.backend.set_gravity(env, gravity)
