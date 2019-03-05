771309b2dd69d3ecf883b0c075f41dfce41eb03a
Sharad Vikram <sharad.vikram@gmail.com>
2019-03-04 21:48:39 -0800

    sampling inplace

--- dr/dist/env_dist.py
+++ dr/dist/env_dist.py
@@ -10,11 +10,17 @@ class EnvironmentDistribution(object, metaclass=ABCMeta):
         self.env_name, self.backend_name = env_name, backend_name
         self.backend = get_backend(backend_name)
         self.env_driver = get_driver(env_name, self.backend)
+        self.root_env = self.backend.make(self.env_name)
+        self.root_env.env.disableViewer = False
+        self.default_parameters = self.env_driver.get_parameters(self.root_env)
         self._seed = None
 
-    def sample(self):
-        env = self.backend.make(self.env_name)
-        env.env.disableViewer = False
+    def sample(self, in_place=True):
+        if self.in_place:
+            env = self.root_env
+        else:
+            env = self.backend.make(self.env_name)
+            env.env.disableViewer = False
         parameters = self.env_driver.get_parameters(env)
         parameters = self._sample(parameters)
         self.env_driver.set_parameters(env, parameters)
