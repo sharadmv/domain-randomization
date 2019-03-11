c8ca6eca1fdc42fe9a3b341f6b3d475091fe81b4
Quan Vuong <quan.vuong@nyu.edu>
2019-03-10 22:36:11 -0700

    add script to run experiments on multiple seeds

---
+++ run_experiments.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for ((i=5;i<15;i+=1))
+do
+    docker run --detach \
+     -v $(pwd):/root/work/domain-randomization -it sharadmv/domain-randomization
+     pipenv run python scripts/ppo.py  --seed $i
+done
