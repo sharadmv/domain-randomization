c4e47e80862a96065004bbadfe222bdbf37b8c1c
Quan Vuong <quan.vuong@nyu.edu>
2019-03-11 17:32:39 -0700

    for ((i=15;i<30;i+=1))

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for ((i=5;i<15;i+=1))
+for ((i=15;i<30;i+=1))
 do
     docker run --detach \
      -v $(pwd):/root/work/domain-randomization -it sharadmv/domain-randomization \
