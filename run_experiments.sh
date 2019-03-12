6e58d8c888b2cf5a393f53170fd45abca417a477
Quan Vuong <quan.vuong@nyu.edu>
2019-03-11 23:56:08 -0700

    remove stdev on env

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,5 +4,5 @@ for ((i=0;i<15;i+=1))
 do
     docker run --detach \
      -v $(pwd):/root/work/domain-randomization -it sharadmv/domain-randomization \
-     pipenv run python scripts/ppo.py  --seed $i --collision_detector ode --env_dist_stdev 0.5
+     pipenv run python scripts/ppo.py  --seed $i --collision_detector ode
 done
