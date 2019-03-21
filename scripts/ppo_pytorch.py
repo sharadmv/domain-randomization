2e4ffb3be2c52061ee956101f857fcbbd3f480c2
Quan Vuong <quan.vuong@nyu.edu>
2019-03-20 17:57:11 -0700

    @click.option('--num_timesteps', type=int, default=20000)

--- scripts/ppo_pytorch.py
+++ scripts/ppo_pytorch.py
@@ -18,7 +18,7 @@ branch = repo.active_branch.name
 @click.option('--env_name', type=str, default='Hopper')
 @click.option('--backend', type=str, default='dart')
 @click.option('--collision_detector', type=str, default='bullet')
-@click.option('--num_timesteps', type=int, default=100000)
+@click.option('--num_timesteps', type=int, default=20000)
 @click.option('--seed', type=int, default=0)
 @click.option('--env_dist_stdev', type=float, default=0.0)
 @click.option('--mean_scale', type=float, default=1.0)
